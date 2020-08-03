3e0514842998ad48cca0a0398ba8de2e4a129808
seanhart6708 <seanmhart24@gmail.com>
2020-08-02 20:45:37 -0400

    optional byline

--- src/components/Article/Article.js
+++ src/components/Article/Article.js
@@ -1,14 +1,30 @@
 import React from 'react';
 
 class Article extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.checkByline = this.checkByline.bind(this);
+    }
+
     // The article component takes the articles from article list and displays their information
+
+    checkByline() {
+        if (this.props.article.byline) {
+            return (
+                <span>{this.props.article.byline} | </span>
+            )
+        }
+        return;
+    }
+
     render() {
+
         return (
             <div>
                 <h4>{this.props.article.title}</h4>
-                {/*<a href={this.props.article.url}>View Article</a>*/}
-                <p>{this.props.article.byline} | {this.props.article.section} | {this.props.article.publishedDate}</p>
-                <a href={this.props.article.url} target='_blank'>Read Article</a>
+                <p>{this.checkByline(this.props.article)}{this.props.article.section} | {this.props.article.publishedDate}</p>
+                <a href={this.props.article.url} target='_blank' rel="noopener noreferrer">Read Article</a>
             </div>
         )
     }
